22ad739e88ada332a732951f5433e6ebcd30a88b
Manoel <125422009+ManoelMorais@users.noreply.github.com>
2024-03-02 13:14:57 -0300

    65% completed

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,15 +1,49 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '../../../types';
 import { RatingModule } from 'primeng/rating';
 import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmationService } from 'primeng/api';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [ RatingModule, FormsModule ],
+  imports: [
+    RatingModule,
+    FormsModule,
+    ButtonModule,
+    ConfirmPopupModule,
+    ToastModule,
+  ],
+  providers: [ConfirmationService],
   templateUrl: './product.component.html',
-  styleUrl: './product.component.scss'
+  styleUrl: './product.component.scss',
 })
 export class ProductComponent {
+  constructor(private confirmationService: ConfirmationService) {}
+
   @Input() product!: Product;
+  @Output() edit: EventEmitter<Product> = new EventEmitter<Product>();
+  @Output() delete: EventEmitter<Product> = new EventEmitter<Product>();
+
+  editProduct() {
+    this.edit.emit(this.product);
+  }
+
+  confirmDelete() {
+    this.confirmationService.confirm({
+      message: 'Are you sure that you want to proceed?',
+      accept: () => {
+        this.deleteProduct();
+      },
+    });
+  }
+
+  deleteProduct() {
+    this.delete.emit(this.product);
+  }
+
+  ngOnInit() {}
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,20 @@ import { ProductsService } from '../services/products.service';
 import { Products, Product } from '../../types';
 import { ProductComponent } from '../components/product/product.component';
 import { CommonModule } from '@angular/common';
+import { PaginatorModule } from 'primeng/paginator';
+import { EditPopupComponent } from '../components/edit-popup/edit-popup.component';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductComponent, CommonModule],
+  imports: [
+    ProductComponent,
+    CommonModule,
+    PaginatorModule,
+    EditPopupComponent,
+    ButtonModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -16,11 +25,111 @@ export class HomeComponent {
 
   products: Product[] = [];
 
-  ngOnInit() {
+  totalRecords: number = 0;
+  rows: number = 5;
+
+  displayEditPopup: boolean = false;
+  displayAddPopup: boolean = false;
+
+  toggleEditPopup(product: Product) {
+    this.selectedProduct =  product;
+    this.displayEditPopup = true;
+  }
+
+  toggleDeletePopup(product: Product) {
+    this.selectedProduct =  product;
+  }
+
+  toggleAddPopup() {
+    this.displayAddPopup = true;
+  }
+
+  selectedProduct: Product = {
+    id: 0,
+    name: '',
+    image: '',
+    price: '',
+    rating: 0,
+  }
+
+  onConfirmEdit(product: Product) {
+    if (!this.selectedProduct.id) {
+      return;
+    }
+
+    this.displayEditPopup = false;
+    this.aditProduct(product, this.selectedProduct.id);
+  }
+
+  onConfirmAdd(product: Product) {
+    this.displayAddPopup = false;
+    this.addProduct(product);
+  }
+
+  onProducOutput(product: Product) {
+    console.log(product, 'product');
+  }
+
+  onPageChange(event: any) {
+    this.fetchProducts(event.page, event.rows);
+  }
+
+  fetchProducts(page: number, perPage: number) {
     this.productsService
-      .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
-      .subscribe((products: Products) => {
-        this.products = products.items;
+      .getProducts('http://localhost:3000/clothes', {
+        page,
+        perPage,
+      })
+      .subscribe({
+        next: (data: Products) => {
+          this.products = data.items;
+          this.totalRecords = data.total;
+        },
+        error: (err) => {
+          console.error(err);
+        }
       });
+  }
+
+  aditProduct(product: Product, id: number) {
+    this.productsService
+      .editProduct(`http://localhost:3000/clothes/{id}`, product)
+      .subscribe({
+        next: (data) => {
+          console.log(data);
+          this.fetchProducts(0, this.rows);
+        },
+        error: (err) => {
+          console.error(err);
+        },
+      });
+  }
+
+  deleteProduct(product: Product, id: number) {
+    this.productsService.deleteProduct(`http://localhost:3000/clothes/{id}`).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.fetchProducts(0, this.rows);
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    })
+  }
+
+  addProduct(product: Product) {
+    this.productsService.addProduct(`http://localhost:3000/clothes`, product).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.fetchProducts(0, this.rows);
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    })
+  }
+
+  ngOnInit() {
+    this.fetchProducts(0, this.rows);
   }
 }
